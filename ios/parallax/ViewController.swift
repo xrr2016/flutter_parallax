50d19817bcd37ae886ddf1cdb070cb8a7e2a5740
轻剑快马 <xiaoranran1993@outlook.com>
2019-06-30 23:11:30 +0800

    Initial commit

---
+++ ios/parallax/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  parallax
+//
+//  Created by xiaoranran on 2019/6/30.
+//  Copyright © 2019 The Chromium Authors. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
